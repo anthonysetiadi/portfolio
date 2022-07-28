959c19bc8d55eaf4245223266dd1211b30adc52b
Anthony Setiadi <setiadi.anthony@gmail.com>
2022-07-28 18:54:39 +1000

    initial commit and navbar

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+
+// Icons
+import MenuIcon from '@mui/icons-material/Menu'
+import CloseIcon from '@mui/icons-material/Close'
+
+import antzLogo from '../../public/AntzLogo.svg'
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => setShowMenu(!showMenu)
+  function handleCloseMenu() {
+    setShowMenu(false)
+  }
+
+  const menuItems = [
+    {
+      type: 'link',
+      title: {
+        name: 'Projects'
+      },
+      link: '/projects'
+    },
+    {
+      type: 'link',
+      title: {
+        name: 'About'
+      },
+      link: '/about'
+    },
+    {
+      type: 'link',
+      title: {
+        name: 'Contact'
+      },
+      link: '/contact'
+    }
+  ]
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.wrapper}>
+        <div
+          className={styles.left}
+          onClick={() => {
+            handleCloseMenu()
+          }}
+        >
+          <Image
+            className={styles.logo}
+            alt="Antz Logo"
+            src={antzLogo}
+            width={24}
+            height={24}
+          />
+          <h1 className={styles.title}>
+            antz.<span>dev</span>
+          </h1>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.mobileMenu} onClick={toggleMenu}>
+            {showMenu ? <CloseIcon /> : <MenuIcon />}
+          </div>
+          <nav
+            className={[
+              styles.menuWrapper,
+              showMenu ? styles.menuOpen : styles.menuClose
+            ].join(' ')}
+          >
+            {menuItems.map(item => (
+              <a key={item.title.name} href={item.link}>
+                {item.title.name}
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,67 @@
+import Head from 'next/head'
+
+import Layout from '../components/Layout/Layout'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Portfolio({ Component, pageProps }) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || (page => <Layout>{page}</Layout>)
+  const content = getLayout(<Component {...pageProps} />)
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" href="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="title" content="Anthony Setiadi Portfolio" />
+        <meta
+          name="description"
+          content="Anthony Setiadi is a front-end developer who specializes in building web applications."
+        />
+        <meta name="keywords" content="Anthony Setiadi, Portfolio, Developer" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://antz.dev" />
+        <meta
+          property="og:title"
+          content="Anthony Setiadi is a front-end developer who specializes in building web applications."
+        />
+        <meta
+          property="og:description"
+          content="Anthony Setiadi is a front-end developer who specializes in building web applications."
+        />
+        {/* <meta property='twitter:card' content='summary_large_image' />
+				<meta property='twitter:url' content='https://www.wunderkid.com.au' />
+				<meta
+					property='twitter:title'
+					content='Exciting activities for kids to discover their true talent.'
+				/>
+				<meta
+					property='twitter:description'
+					content='Find the best activities for kids and discover their true talent - all made possible by providers on Wunderkid.'
+				/> */}
+        {/* Things to be filled later
+				<meta
+					property="og:image"
+					content="https://www.flytree.co/images/Flytree-OG-Image.jpg"
+				/>
+				<meta
+					property="twitter:image"
+					content="https://www.flytree.co/images/Flytree-OG-Image.jpg"
+				/>
+				<link
+					rel="preload"
+					as="image"
+					href="https://ik.imagekit.io/flytree/Flytree-front-page_199DWSYmX.webp"
+				/>
+				*/}
+        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <title>Anthony Setiadi - Homepage</title>
+      </Head>
+      {content}
+    </>
+  )
 }
 
-export default MyApp
+export default Portfolio
