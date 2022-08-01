5727237c24b2c2c434b57e4bff0da85cd1ee65ec
Anthony Setiadi <setiadi.anthony@gmail.com>
2022-08-01 21:47:41 +1000

    head: updated meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -36,51 +36,21 @@ function Portfolio({ Component, pageProps }) {
         <meta name="msapplication-TileColor" content="#1a202c" />
         <meta name="theme-color" content="#f8f8fa" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="title" content="Anthony Setiadi Portfolio" />
-        <meta
-          name="description"
-          content="Anthony Setiadi is a front-end developer who specializes in building web applications."
-        />
+        <meta name="title" content="Anthony Setiadi" />
+        <meta name="description" content="Anthony Setiadi" />
         <meta name="keywords" content="Anthony Setiadi, Portfolio, Developer" />
         <meta name="robots" content="index, follow" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
+        <meta property="og:title" content="Anthony Setiadi" />
         <meta property="og:type" content="website" />
+        <meta property="og:description" content="Anthony Setiadi" />
+        <meta
+          property="og:image"
+          content="https://ik.imagekit.io/antz/DSC00410_1__San4RSY04.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657780111677"
+        />
         <meta property="og:url" content="https://antz.dev" />
-        <meta
-          property="og:title"
-          content="Anthony Setiadi is a front-end developer who specializes in building web applications."
-        />
-        <meta
-          property="og:description"
-          content="Anthony Setiadi is a front-end developer who specializes in building web applications."
-        />
-        {/* <meta property='twitter:card' content='summary_large_image' />
-				<meta property='twitter:url' content='https://www.wunderkid.com.au' />
-				<meta
-					property='twitter:title'
-					content='Exciting activities for kids to discover their true talent.'
-				/>
-				<meta
-					property='twitter:description'
-					content='Find the best activities for kids and discover their true talent - all made possible by providers on Wunderkid.'
-				/> */}
-        {/* Things to be filled later
-				<meta
-					property="og:image"
-					content="https://www.flytree.co/images/Flytree-OG-Image.jpg"
-				/>
-				<meta
-					property="twitter:image"
-					content="https://www.flytree.co/images/Flytree-OG-Image.jpg"
-				/>
-				<link
-					rel="preload"
-					as="image"
-					href="https://ik.imagekit.io/flytree/Flytree-front-page_199DWSYmX.webp"
-				/>
-				*/}
-        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <meta name="twitter:card" content="summary_large_image" />
         <title>Anthony Setiadi - Homepage</title>
       </Head>
       {content}
